4ced57c0ec328754d9f3dc79cd0fc6afedae24b1
Mirac Dogan <miracdogn@gmail.com>
2022-02-08 00:10:26 +0300

    Create a password with button
    With this commit, you can create a 20 letter long password.

--- ViewController.swift
+++ ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var CreateButton: UIButton!
+    @IBOutlet var textView: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        var backgroundColor = UIColor(red: 0.29, green: 0.42, blue: 0.46, alpha: 1.00)
+        var buttonColor = UIColor(red: 0.82, green: 0.95, blue: 0.97, alpha: 1.00)
+        
+        textView.isUserInteractionEnabled = false
+        self.view.backgroundColor = backgroundColor
+        CreateButton.backgroundColor = buttonColor
+        CreateButton.layer.cornerRadius = 10
+        
     }
 
+    func generateRandomPassword(of length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        var password = ""
+        for _ in 0 ..< length {
+            password.append(letters.randomElement()!)
+        }
+        return password
+        
+    }
+    
+    @IBAction func onChangeTextButton(_ CreateButton: UIButton){
+        textView.textAlignment = .center
+        textView.text = generateRandomPassword(of: 20)
+    }
 
 }
 
